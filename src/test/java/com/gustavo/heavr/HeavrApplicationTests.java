c22253f85f210f5fc63169ffd710d39fcdf7f509
Gustavo Leite <qustavoleite@gmail.com>
2026-05-26 11:07:48 -0300

    initial commit

---
+++ src/test/java/com/gustavo/heavr/HeavrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.heavr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeavrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
