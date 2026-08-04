aa99ccf7affe924cf93590712c446acc23d077fa
Gustavo Leite <qustavoleite@gmail.com>
2026-08-04 12:33:08 -0300

    feat: add findByIdAndUserId method to WorkoutRepository

--- src/main/java/com/gustavo/heavr/repository/WorkoutRepository.java
+++ src/main/java/com/gustavo/heavr/repository/WorkoutRepository.java
@@ -4,8 +4,11 @@ import com.gustavo.heavr.entity.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface WorkoutRepository extends JpaRepository<Workout, Long> {
     List<Workout> findByUserId(Long userId);
+
+    Optional<Workout> findByIdAndUserId(Long id, Long userId);
 }
